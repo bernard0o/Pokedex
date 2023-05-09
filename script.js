dacce89a99a568c89c574643a578c6ab28b192df
Bernardo Carvalho De Azeredo <2222095@alunos.escoladigital.com>
2023-05-09 12:22:43 +0100

    .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const url = "https://pokeapi.co/api/v2/pokemon/";
+const blueButton = document.getElementById("bigbluebutton");
 
 let c = 1;
 
@@ -19,6 +20,35 @@ function getDataPast(){
     }
 }
 
+//Lista de pokémons ao clicar no botão azul
+
+blueButton.addEventListener("click", async function(){
+        document.getElementById("pokemonList").style.display = "block"
+        for (let i = 1; i <= 1030; i++){
+            let connect = await fetch((url + i).toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ""));
+            let data = await connect.json();
+            let div = document.createElement("div");
+            div.className = "pokemon";
+
+            let imagem = document.createElement("img");
+            imagem.className = "img";
+            imagem.src = await data.sprites.front_default;
+            div.appendChild(imagem)
+
+            let h2 = document.createElement("h2");
+            div.append(h2);
+
+            h2.textContent = await String(data["name"]).charAt(0).toUpperCase() + String(data["name"]).slice(1);
+            document.getElementById("bottom").appendChild(div)
+        }
+    })
+    document.getElementById("back").addEventListener("click", function(){
+        document.getElementById("pokemonList").style.display = "none";
+    })
+
+//Search Bar
+
+//Buscar dados à API
 async function getData(){
     let getName = document.getElementById("pokemonName").value;
     const nome = document.getElementById("nome");
